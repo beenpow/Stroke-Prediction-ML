36a5203fb4ed467d63c3528898700c912a3ada86
Emily Weiss <emilymwe@usc.edu>
2026-03-29 22:45:45 -0700

    add stratify to balance class labels

--- stroke_data.py
+++ stroke_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from collections import Counter
 
 def get_stroke_data():
     # load in imputed stroke data using k=5 and distance weights 
@@ -15,26 +16,11 @@ def get_stroke_data():
     # transform features and labels into numpy arrays and check shape 
     X = X_df.to_numpy()
     y = y_df.to_numpy()
-    # print("Shape of encoded data with stroke col dropped: ", X.shape)
-    # print("Shape of encoded data with stroke col dropped: ", y.shape)
 
     # Do an initial 80/20 split
-    X_train, X_temp_test, y_train, y_temp_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    X_train, X_temp_test, y_train, y_temp_test = train_test_split(X, y, test_size=0.20, random_state=42, stratify=y)
 
-    # print("X_train shape = ", X_train.shape)
-    # print("X_temp_test shape = ", X_temp_test.shape)
-    # print("y_train shape = ", y_train.shape)
-    # print("y_temp_test shape = ", y_temp_test.shape)
-
-    # print("Checking sum of elems = ", X_train.shape[0] + X_temp_test.shape[0])
-
-    half_idx = int(X_temp_test.shape[0] / 2)
-
-    # get the val and test splits by splitting the temp_test numpy arrays at the half_idx
-    X_val = X_temp_test[:half_idx]
-    X_test = X_temp_test[half_idx:]
-
-    y_val = y_temp_test[:half_idx]
-    y_test = y_temp_test[half_idx:]
+    # Do second split to get the val and test splits from the 20
+    X_val, X_test, y_val, y_test = train_test_split(X_temp_test, y_temp_test, test_size=0.5, random_state=42, stratify=y_temp_test)
 
     return X_train, X_val, X_test, y_train, y_val, y_test
