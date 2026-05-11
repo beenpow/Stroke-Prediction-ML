6753029bd619d85ef16e48c461f1094369bf482d
Emily Weiss <emilymwe@usc.edu>
2026-05-11 15:28:05 -0700

    remove function calls in weighted_baselines

--- src/weighted_baselines.py
+++ src/weighted_baselines.py
@@ -165,6 +165,3 @@ def run_weighted_baselines(X_train, X_test, y_train, y_test):
     # results['knn'] = k_nearest_neighbors(X_train, X_test, y_train, y_test)
 
     return results
-
-X_train, X_test, y_train, y_test = get_stroke_data_for_cv("data/knn-standardize-distance.csv")
-pprint(run_all_baselines(X_train, X_test, y_train, y_test))
